c97a88761fb4fcb21bc2a1789c9d28924acf663f
George Juarez <jjuarez@albany.edu>
2016-07-01 16:19:46 -0400

    Update GUIProgramming.py

--- GUIProgramming.py
+++ GUIProgramming.py
@@ -1,5 +1,7 @@
 # George Juarez
 
+import tkinter
+
 # 14.2
 # tkinter module ("TK interface") is used to create simple GUI programs
 # comes with widgets that the user can interact with or view
@@ -16,15 +18,29 @@ class MyGUI:
 
 myGui = MyGUI()
 
-'''
-# 
+''' 
 
-import tkinter
+class MySampleLabelGUI:
+    
+    def __init__(self):
+        # create main window widget
+        self.main_window = tkinter.Tk()
 
+        # create Label widget with Hellow World
+        # first param (self.main_window) is a reference to the root widget
+        
+        self.label1 = tkinter.Label(self.main_window, text = "Hello World!")
+        self.label2 = tkinter.Label(self.main_window, text = "Memes are cool!")
+        
+        # call Label's pack method
+        self.label1.pack(side = 'left')
+        self.label2.pack(side = 'left')
+
+        tkinter.mainloop()
+
+    
 def main():
-    main_window = tkinter.Tk()
-
-    tkinter.mainloop()
+    myLabelGUI = MySampleLabelGUI()
 
 # call main
 main()
